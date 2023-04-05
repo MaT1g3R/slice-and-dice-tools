6c712cde44ee0f30bd95afe4e119a27c7f3a075b
Peijun Ma <peijun.ma@protonmail.com>
2023-04-04 22:53:15 -0400

    Be smarter about watching for save file changes
    commit-id:18e77d0d

--- display_curse/display_curse.py
+++ display_curse/display_curse.py
@@ -1,9 +1,44 @@
 import json
+import os
 import re
 import time
 import xml.etree.ElementTree as ET
 from argparse import ArgumentParser
 from pathlib import Path
+
+
+class FileWatcher:
+    def __init__(self, path):
+        self.ts = 0
+        self.path = path
+
+    def updated(self):
+        stamp = os.stat(self.path).st_mtime
+        if stamp != self.ts:
+            self.ts = stamp
+            return True
+        return False
+
+    def watch(self, fn):
+        while True:
+            if self.updated():
+                print(f"{self.path} changed")
+                fn()
+            time.sleep(1)
+
+
+def write_curses(file, out_file, gamemode):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    for node in root:
+        tag = node.tag
+        attrib = node.attrib
+
+        if tag == "entry" and attrib.get("key") == gamemode:
+            js = json.loads(node.text)
+            curses = js.get("d", {}).get("m", [])
+            curses = [re.sub(r"\[\w+\]", "", curse) for curse in curses]
+            out_file.write_text("\n".join(curses))
 
 
 def main():
@@ -35,24 +70,8 @@ def main():
     out_file = args.output
     gamemode = args.gamemode
 
-    with open(in_file, "r") as in_fd:
-        while True:
-            tree = ET.parse(in_fd)
-            root = tree.getroot()
-            for node in root:
-                tag = node.tag
-                attrib = node.attrib
-
-                if tag == "entry" and attrib.get("key") == gamemode:
-                    js = json.loads(node.text)
-                    curses = js.get("d", {}).get("m", [])
-                    curses = [re.sub(r"\[\w+\]", "", curse) for curse in curses]
-                    out_file.write_text("\n".join(curses))
-                    break
-
-            in_fd.seek(0)
-            time.sleep(1)
-
+    watcher = FileWatcher(in_file)
+    watcher.watch(lambda:  write_curses(in_file, out_file, gamemode))
 
 if __name__ == "__main__":
     main()
