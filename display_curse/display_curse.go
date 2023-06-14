de30e54109a3ec981b4a50e406466fe2686ae307
Peijun Ma <peijun.ma@protonmail.com>
2023-06-13 23:42:37 -0400

    Add modifier tier to display_curse output

--- display_curse/display_curse.go
+++ display_curse/display_curse.go
@@ -1,17 +1,78 @@
 package main
 
 import (
+	_ "embed"
+	"encoding/csv"
 	"encoding/json"
 	"encoding/xml"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
+
+//go:embed curse.csv
+var curseCSV string
+
+//go:embed tweak.csv
+var tweakCSV string
+
+//go:embed blessing.csv
+var blessingCSV string
+
+type Modifier struct {
+	Name string
+	Tier int
+}
+
+func loadModifier(reader io.Reader) ([]Modifier, error) {
+	var mods []Modifier
+	r := csv.NewReader(reader)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		tier, err := strconv.Atoi(record[1])
+		if err != nil {
+			return nil, err
+		}
+		mods = append(mods, Modifier{Name: normalizeName(record[0]), Tier: tier})
+	}
+	return mods, nil
+}
+
+func LoadModifiers() ([]Modifier, error) {
+	var modifiers []Modifier
+	curses, err := loadModifier(strings.NewReader(curseCSV))
+	if err != nil {
+		return nil, err
+	}
+	modifiers = append(modifiers, curses...)
+
+	tweaks, err := loadModifier(strings.NewReader(tweakCSV))
+	if err != nil {
+		return nil, err
+	}
+	modifiers = append(modifiers, tweaks...)
+
+	blessings, err := loadModifier(strings.NewReader(blessingCSV))
+	if err != nil {
+		return nil, err
+	}
+	modifiers = append(modifiers, blessings...)
+
+	return modifiers, nil
+}
 
 type FileWatcher struct {
 	ts   int64
@@ -23,6 +84,10 @@ func NewFileWatcher(path string) *FileWatcher {
 		ts:   0,
 		path: path,
 	}
+}
+
+func normalizeName(name string) string {
+	return regexp.MustCompile(`\[\w+]`).ReplaceAllString(name, "")
 }
 
 func (fw *FileWatcher) updated() bool {
@@ -58,15 +123,15 @@ type Entry struct {
 	Value string `xml:",innerxml"`
 }
 
-type CursesData struct {
-	D Curses `json:"d"`
+type Data struct {
+	D Modifiers `json:"d"`
 }
 
-type Curses struct {
+type Modifiers struct {
 	M []string `json:"m"`
 }
 
-func writeCurses(file string, out_file string, gamemode string) {
+func writeCurses(file string, out_file string, gamemode string, modifierTiers map[string]int) {
 	xmlFile, err := os.Open(file)
 	if err != nil {
 		fmt.Println(err)
@@ -74,7 +139,7 @@ func writeCurses(file string, out_file string, gamemode string) {
 	}
 	defer xmlFile.Close()
 
-	byteValue, err := ioutil.ReadAll(xmlFile)
+	byteValue, err := io.ReadAll(xmlFile)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -87,20 +152,26 @@ func writeCurses(file string, out_file string, gamemode string) {
 		return
 	}
 
-	var curses []string
+	var modifiers []string
 	for _, entry := range prefs.Entries {
 		if entry.Key == gamemode {
-			var data CursesData
+			var data Data
 			json.Unmarshal([]byte(entry.Value), &data)
-			for _, curse := range data.D.M {
-				curses = append(curses, regexp.MustCompile(`\[\w+\]`).ReplaceAllString(curse, ""))
+			for _, modifier := range data.D.M {
+				modifier = normalizeName(modifier)
+				tier, ok := modifierTiers[modifier]
+				if ok {
+					modifiers = append(modifiers, fmt.Sprintf("[%d] %s", tier, modifier))
+				} else {
+					modifiers = append(modifiers, modifier)
+				}
 			}
 			break
 		}
 	}
 
-	out := strings.Join(curses, "\n")
-	err = ioutil.WriteFile(out_file, []byte(out), os.ModePerm)
+	out := strings.Join(modifiers, "\n")
+	err = os.WriteFile(out_file, []byte(out), os.ModePerm)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -115,9 +186,19 @@ func main() {
 
 	if *output == "" {
 		fmt.Println("Output path must be specified")
-		return
+		os.Exit(1)
+	}
+
+	modifiers, err := LoadModifiers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	modifierTiers := make(map[string]int)
+	for _, mod := range modifiers {
+		modifierTiers[mod.Name] = mod.Tier
 	}
 
 	watcher := NewFileWatcher(*input)
-	watcher.watch(func() { writeCurses(*input, *output, *gamemode) })
+	watcher.watch(func() { writeCurses(*input, *output, *gamemode, modifierTiers) })
 }
